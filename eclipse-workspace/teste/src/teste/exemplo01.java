c3ad7d65530304d05d620e7fe251de4bb690e8d1
vitorlescola <158179013+vitorlescola@users.noreply.github.com>
2024-05-27 11:59:29 -0300

    Add files via upload

---
+++ eclipse-workspace/teste/src/teste/exemplo01.java
@@ -0,0 +1,19 @@
+package teste;
+
+public class exemplo01 {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		System.out.println("ln cria na proxima linha");
+		System.out.println("sem ln cria na mesma");
+		
+		String primeiroNome="Maria";
+		String sobrenome="Silva";
+		int idade=40;
+		char sexo='F';
+		float salario=10245.56f;
+		
+		System.out.println(primeiroNome + sobrenome);
+		System.out.println(primeiroNome + idade);
+		System.out.println(idade + salario);
+	}
+}
